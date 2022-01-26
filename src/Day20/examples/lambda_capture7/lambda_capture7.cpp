6ac938602c1c0fc6ce5693b16a0e3485cb3d47be
Manoj Joshi <manoj.joshi@smoothstack.com>
2022-01-26 19:42:20 +0000

    fixed the code

--- src/Day20/examples/lambda_capture7/lambda_capture7.cpp
+++ src/Day20/examples/lambda_capture7/lambda_capture7.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
 #include <cassert>
  
 int main() {
-    int a = 1, b = 1, c = 1;
+    int a = 1, b = 1, c = 1; // a always passed by value == 1
  
-    auto m1 = [a, &b, &c]() mutable {
-        auto m2 = [a, b, &c]() mutable -> int {
+    auto m1 = [a, &b, &c]() mutable { // a==1, b==2, c==2
+        auto m2 = [a, b, &c]() mutable -> int { // a==1, b==2, c==3
             auto m = a + b + c;
             a = 4; b = 4; c = 4;
             return m;
         };
-        a = 3; b = 3; c = 3;
+        a = 3; b = 3; c = 3; // c gets set to 3 (passed by ref)
         assert(m2() == 6);
     };
  
